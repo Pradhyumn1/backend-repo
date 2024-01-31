39b6f9ae8bee87bc2502634be39019de5f1cf312
Pradhyumn Yadav <73153956+Pradhyumn1@users.noreply.github.com>
2024-01-31 06:12:29 +0530

    / route added

--- main.py
+++ main.py
@@ -27,7 +27,9 @@ app.add_middleware(
 MODEL = tf.keras.models.load_model("models/1")
 
 CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
-
+@app.get("/")
+async def  root():
+     return {"Message": "backend is runnig here for my ml project"}
 @app.get("/ping")
 async def ping():
     return "Hello, I am alive"
